ebda21067258490eec41e74f5192d046ca657b3f
Marius C. Bolik <marius@eyloo.com>
2023-06-13 22:39:45 +0200

    chore: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: 'server',
+  integrations: [react()],
+  adapter: cloudflare()
 });
