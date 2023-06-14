07c7b3e3c49a04e423bc05711accbf2ac2072a9d
Marius C. Bolik <marius@eyloo.com>
2023-06-14 11:01:58 +0200

    build: integrate vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [react()],
-  adapter: cloudflare()
+  adapter: vercel()
 });
